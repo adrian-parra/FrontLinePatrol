9f6881b60e0159469fc15000b8f45ff81203c750
Tu Nombre <tu@email.com>
2024-06-19 23:40:01 -0700

    feat Kaizen
    * Se creo interfaz para poder consultar kaizen y ver pdf en la misma pagina

---
+++ Controllers/KaizenController.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LinePatrol.Models;
+
+namespace LinePatrol.Controllers;
+
+public class KaizenController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public KaizenController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+        [HttpPost]
+    public async Task<IActionResult> Filter(Kaizen kaizen)
+    {
+
+        // Aquí puedes configurar la URL de la API externa
+    string apiUrl = "http://c7share.sewsus.com.mx:9001/Kaizen/MostrarKaizen.php";
+
+    using (var client = new HttpClient())
+    {
+      // Crea un objeto FormUrlEncodedContent con los datos del formulario
+            var formData = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("Fecha1", kaizen.Fecha1),
+                new KeyValuePair<string, string>("Fecha2", kaizen.Fecha2),
+                new KeyValuePair<string, string>("Planta", kaizen.Planta),
+                new KeyValuePair<string, string>("Perfil", kaizen.Perfil),
+                new KeyValuePair<string, string>("Reloj", kaizen.Reloj),
+
+            });
+
+            // Envía la solicitud POST con el contenido del formulario
+            var response = await client.PostAsync(apiUrl, formData);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var htmlTable = await response.Content.ReadAsStringAsync();
+            return Content(htmlTable, "text/html");
+        }
+        else
+        {
+            // Maneja el error en caso de que la solicitud a la API falle
+            return StatusCode((int)response.StatusCode, "Error al obtener datos de la API");
+        }
+    }
+
+       
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/kaizen.cs
@@ -0,0 +1,10 @@
+namespace LinePatrol.Models;
+public class Kaizen {
+    public string Fecha1 { get; set; }
+    public string Fecha2 { get; set; }
+    public string Planta { get; set; }
+    public string? Perfil { get; set; } = "0";
+
+    public string? Reloj { get; set; } = "256553";
+
+}
